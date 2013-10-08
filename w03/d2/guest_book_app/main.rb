a265c1781be490da4a2775b765f34b9d9e6a0a83
Quin Cogdell <quincogdell@gmail.com>
2013-10-08 19:30:31 -0400

    updated classwork

--- w03/d2/guest_book_app/main.rb
+++ w03/d2/guest_book_app/main.rb
@@ -11,17 +11,20 @@ get '/guests' do
   db_conn = PG.connect(:dbname => 'guest_book_db', :host => 'localhost')
   sql = "SELECT * FROM guests"
   @guests = db_conn.exec(sql)
-  binding.pry
+  #binding.pry
   erb :guests
 
 end
 
 post '/guests' do
   #binding.pry
-  name = params["name"]
-  msg = params["msg"]
+  #name = params["name"]
+  #msg = params["msg"]
   db_conn = PG.connect(:dbname => 'guest_book_db', :host => 'localhost')
-  sql = "INSERT INTO guests (name, msg) VALUES ('#{name}', '#{msg}')"
+  sql = "INSERT INTO guests (name, msg) VALUES ('#{params["name"]}', '#{params["msg"]}')"
+
+  #sql = "INSERT INTO guests (name, msg) VALUES ('#{name}', '#{msg}')"
+
   db_conn.exec(sql)
   db_conn.close
   redirect to '/guests'
