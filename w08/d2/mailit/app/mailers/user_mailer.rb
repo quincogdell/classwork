76c909875d6929a64efb19db7cae3c3663eae1d2
Quin Cogdell <quincogdell@gmail.com>
2013-11-12 10:41:21 -0500

    working mailit app

---
+++ w08/d2/mailit/app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ActionMailer::Base
+  default from: "quincogdell@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+  #
+  def signup_confirmation(user)
+    @user = user
+
+
+    mail to: user.email, subject: "Sign Up Confirmation"
+  end
+end
---
+++ w08/d2/mailit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = 'c2cf1b593d1a17c44ac38da8ba00d179bc212604d338c07a804186b7b734c5470a3fca8634dfa75d7fe427023d34b49f0a07038914117818899915870ee30c13'
